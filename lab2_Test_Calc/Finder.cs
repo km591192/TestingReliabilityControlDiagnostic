17066345b2e289f493f97d8579ebf859d403efd7
Kateryna <>
2017-12-07 16:55:08 +0200

    Testing Reliability Control Diagnostic

---
+++ lab2_Test_Calc/Finder.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace lab2_Test_Calc
+{
+    class Finder
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern uint FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern uint FindWindowEx(uint hwndParent, uint hwndChildAfter, string className, string windowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(uint hWnd, int msg, int wParam, int lParam);
+
+        [DllImport("User32.Dll")]
+        public static extern int GetClassName(int hwnd, StringBuilder lpClassName, int nMaxCount);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public static extern int RegisterWindowMessage(string lpString);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = System.Runtime.InteropServices.CharSet.Auto)] //
+        public static extern bool SendMessage(IntPtr hWnd, uint Msg, int wParam, StringBuilder lParam);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SendMessage(int hWnd, int Msg, int wparam, int lparam);
+
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_LBUTTONUP = 0x0202;
+        const int WM_KEYDOWN = 0x0100;
+        const int WM_KEYUP = 0x0101;
+        const int WM_CHAR = 0x0102;
+        const int BM_CLICK = 0x00F5;
+        const int WM_SETTEXT = 0x000C;
+        const int WM_GETTEXT = 0x000D;
+        const int WM_GETTEXTLENGTH = 0x000E;
+
+        // Нажать кнопку с текстом «1» на стандартном калькуляторе 
+        public void pressbtn_standartcalc(string winName)
+        {
+            // находим окно калькулятора, зная window class ("SciCalc"-XP   "CalcFrame"-Win7  )
+            uint calcWnd = FindWindowEx(0, 0, "CalcFrame", null);
+            // находим кнопку по имени окна ("1") и имени класса ("TButton")
+            uint button1 = FindWindowEx(calcWnd, 0, "TButton", winName);
+            // если нашли
+            if (button1 != 0)
+            {
+                // посылаем нажатие кнопки мыши
+                SendMessage(button1, WM_LBUTTONDOWN, 0, 0);
+                SendMessage(button1, WM_LBUTTONUP, 0, 0);
+            }
+        }
+
+        // Нажать кнопку по хэндлу, заданному в окне ввода в шестнадцатеричном формате 
+        public void pressbtn_handle(string HNDL_TEXT)
+        {
+            string s = HNDL_TEXT;//.Lines[0];
+            uint hnd = uint.Parse(s, System.Globalization.NumberStyles.HexNumber);
+            SendMessage(hnd, BM_CLICK, 0, 0);
+        }
+
+
+        string WindowFormName = "", WindowFormClassName = "", WindowElementClassName = "",editClassName = "", strBM_CLICKS ="";
+        int BM_CLICKS;
+
+        public string start()
+        {
+            dict.Clear();
+            return detected_FormName();
+        }
+        // Нажать кнопку с текстом «1» на калькуляторе CALC 
+        public void pressbtn_CALC(string winName)
+        {
+            dict.TryGetValue("WindowFormName", out WindowFormName);
+            dict.TryGetValue("WindowElementClassName", out WindowElementClassName);
+            dict.TryGetValue("WindowFormClassName", out WindowFormClassName);
+            dict.TryGetValue("editClassName", out editClassName);
+
+            //dict.TryGetValue("BM_CLICKS", out strBM_CLICKS);
+            // BM_CLICKS = Convert.ToInt32(strBM_CLICKS);
+
+            // находим кнопку по имени окна ("1") и имени класса ("Button")
+            uint calcWnd = FindWindowEx(0, 0, WindowFormClassName, WindowFormName);
+            uint button1 = FindWindowEx(calcWnd, 0, WindowElementClassName, winName);
+
+            // если нашли
+            if (button1 != 0)
+            {
+                // посылаем нажатие кнопки мыши
+                SendMessage(button1, BM_CLICK, 0, 0);
+            }
+
+        }
+
+        public void RegisterControlforMessages()
+        {
+          RegisterWindowMessage("WM_GETTEXT");
+        }
+        public string GetControlText()
+        {
+            uint calcWnd = FindWindowEx(0, 0, WindowFormClassName, WindowFormName);
+            uint hWnd  = FindWindowEx(calcWnd, 0, editClassName, "");
+          StringBuilder title = new StringBuilder();
+          Int32 size = SendMessage((int)hWnd, WM_GETTEXTLENGTH, 0, 0).ToInt32();
+          if (size > 0)
+          {
+              title = new StringBuilder(size + 1);
+              SendMessage((IntPtr)hWnd,(int)WM_GETTEXT, title.Capacity, title);
+          }
+          return title.ToString();
+        }
+       
+
+        public SortedDictionary<string, string> dict = new SortedDictionary<string, string>();
+        private string detected_FormName()
+        {
+            string text = "";
+            string answer = "";
+            using (StreamReader fs = new StreamReader("script.txt", Encoding.Default))
+            {
+                while (true)
+                {
+                    string temp = fs.ReadLine();
+                    if (temp == null) break;
+                    text += temp + "\n";
+                }
+            }
+            string[] textline = text.Trim().Split('\n');
+            for (int i = 0; i < textline.Length; i++)
+            {
+                if (new Regex("detect").Matches(textline[i].Trim()).Count == 1)
+                    //textline[i].Trim() == "detect") 
+                {
+                    i++;
+                    while (new Regex("enddetect").Matches(textline[i].Trim()).Count != 1)
+                       // textline[i + 1].Trim() != "enddetect;")
+                    {
+                        string [] temp = textline[i].Trim().Split(':');
+                        dict.Add(temp[0], temp[1]);
+                        i++;
+                    }
+                }
+
+                if (new Regex("expressions").Matches(textline[i].Trim()).Count == 1)
+                //textline[i].Trim() == "detect") 
+                {
+                    i++;
+                    while (new Regex("endexpressions").Matches(textline[i].Trim()).Count != 1)
+                    // textline[i + 1].Trim() != "enddetect;")
+                    {
+                        answer += textline[i].Trim();
+                        answer += "C";
+                        i++;
+                    }
+                }
+
+                if (new Regex("pressbtn").Matches(textline[i].Trim()).Count == 1)
+                {
+                    i++;
+                    while (new Regex("endpressbtn").Matches(textline[i].Trim()).Count != 1)
+                    {
+                        answer += textline[i].Trim();
+                        i++;
+                    }
+                    answer += "C";
+                }
+            }
+            return answer;
+        }
+
+       
+    }
+}
---
+++ lab2_Test_Calc/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2_Test_Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Finder finder = new Finder();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("2");
+            finder.pressbtn_CALC("3");
+            finder.pressbtn_CALC("4");
+            finder.pressbtn_CALC("5");
+            finder.pressbtn_CALC("6");
+            finder.pressbtn_CALC("7");
+            finder.pressbtn_CALC("8");
+            finder.pressbtn_CALC("9");
+            finder.pressbtn_CALC("0");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC(textBox1.Text.ToString().Trim());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("C");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("-");
+            finder.pressbtn_CALC("*");
+            finder.pressbtn_CALC("/");
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("EXIT");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("1");
+                finder.pressbtn_CALC(i.ToString());
+            }
+                for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("2");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("3");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("4");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("5");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("6");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("7");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("8");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("9");
+                finder.pressbtn_CALC(i.ToString());
+            }
+
+            finder.pressbtn_CALC("100");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("1");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("2");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("3");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("4");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("5");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("6");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("7");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("8");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                finder.pressbtn_CALC("9");
+                finder.pressbtn_CALC(i.ToString());
+            }
+
+            finder.pressbtn_CALC("100");
+            finder.pressbtn_CALC("-");
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                finder.pressbtn_CALC(i.ToString());
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                finder.pressbtn_CALC("1");
+                finder.pressbtn_CALC(i.ToString());
+            }
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("C");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("-");
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC("=");
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("*");
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC("=");
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("/");
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("2");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("3");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("-");
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("7");
+            finder.pressbtn_CALC("5");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("2");
+            finder.pressbtn_CALC("4");
+            finder.pressbtn_CALC("9");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC("-");
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("2");
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("3");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("4");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            finder.pressbtn_CALC("0");
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("1");
+            finder.pressbtn_CALC(".");
+            finder.pressbtn_CALC("2");
+            finder.pressbtn_CALC("+");
+            finder.pressbtn_CALC("3");
+            finder.pressbtn_CALC("=");
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string str = finder.start();
+            string data = "";
+            int flag = -1;
+            char[] b = str.ToCharArray();
+           // foreach (char x in b)
+            for (int i = 0; i < b.Length;i++ )
+            {
+                if (b[i] == '/' && b[i+1] == '0' && b[i+2] == '=')
+                {
+                    flag = 1;
+                }
+                if (b[i] != 67)
+                    data += b[i].ToString();
+                if (b[i] == 67 && flag != 1)
+                    data += finder.GetControlText() + "\n";
+                if (b[i] == 67 && flag == 1)
+                {
+                    finder.pressbtn_CALC("C");
+                    data += "\n";
+                    flag = -1;
+                }
+                finder.pressbtn_CALC(b[i].ToString());
+            }
+            string[] dataarr = data.Split('\n');
+            for (int i = 0; i < dataarr.Length; i++)
+            {
+                listBox1.Items.Add(dataarr[i].ToString());
+            }
+        }
+
+    }
+}
